0ea6fb0522207c7f194d2b28f5f94594cd9ed4ae
Gonza10Co <gonzalo_correa_g@hotmail.com>
2022-08-01 16:38:22 -0500

    4

--- client/src/views/about.jsx
+++ client/src/views/about.jsx
@@ -11,10 +11,12 @@ export default function About() {
             Full Stack Web <span>Developer</span>
           </h5>
           <p>
-            My name is Gonzalo Correa. I am a Full Stack web developer. Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Dicta eaque illo necessitatibus natus
-            iure, maxime eum minus similique aliquid perferendis dolor quod
-            laboriosam rem nulla, fugit quaerat aperiam doloribus totam.
+            My name is Gonzalo Correa. I am a Full Stack web developer. Enthusiastic for learning new technologies.<br/>
+            Knowledgment in Javascript, React.js, Redux, Sass, Express, Node JS and
+            Sequelize.<br/>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Dicta eaque illo necessitatibus natus iure, maxime eum minus
+            similique aliquid perferendis dolor quod laboriosam rem nulla, fugit
+            quaerat aperiam doloribus totam.
           </p>
           <button type="button">
             <a href="/dogs/">back</a>
